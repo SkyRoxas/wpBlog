6506e6d509a596e1e8c1b1063d4450ca8368abf5
skyroxas <skyroxas1521@gmail.com>
2017-03-30 21:56:40 +0800

    auther

--- tpl-post/archive_time-line.php
+++ tpl-post/archive_time-line.php
@@ -55,7 +55,7 @@
   <div class ="wrapper <?php echo $another_column.' '.$another_sort.' '.$another_layout?>">
 
     <div class ="article-field avatar">
-      <?php  echo get_avatar(); ?>
+      <?php echo get_avatar(get_the_author_meta('ID'), 100);?>
     </div>
 
   </div>
