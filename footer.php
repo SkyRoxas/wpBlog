bae96cb7d0d9cf0d39bd5f0f10ad47bb9f9a2039
jimmy8646 <yujiechueh@gmail.com>
2017-04-28 01:18:26 +0800

    header and footer refactor by jimmy8646

--- footer.php
+++ footer.php
@@ -3,3 +3,5 @@
     <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
   </div>
 </div>
+<?php do_action('wp_footer');?>
+</body>
--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0,">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style/css/style.css">
+  <?php do_action('wp_head');?>
+  <title><?php wp_title(); ?></title>
+</head>
+<script src="https://use.typekit.net/znw8cer.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<body class ="<?php is_home() ? print'front' : print 'not-front'; ?>">
 <header id ="header">
   <div class ='web-info row center-xs middle-xs'>
     <img class ="logo" src="<?php bloginfo('template_url'); ?>/images/logo.png">
@@ -10,3 +20,4 @@
     <?php wp_nav_menu( array( 'theme_location' => 'header-menu','container_class' => 'header-menu' ) ); ?>
   </div>
 </header>
+<?php get_template_part('tpl/banner') ?>
