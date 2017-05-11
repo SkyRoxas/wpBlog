82e0713b3b565b856cf252c3976bd6b4cd8f2f4f
skyroxas <skyroxas1521@gmail.com>
2017-05-12 07:50:01 +0800

    fix http error

--- functions.php
+++ functions.php
@@ -79,4 +79,13 @@ function add_scripts() {
 
 
 
+  /* Fix wordpress upload http error */
+  function change_graphic_lib($array) {
+	   return array('WP_Image_Editor_GD', 'WP_Image_Editor_Imagick');
+   }
+
+  add_filter('wp_image_editors', 'change_graphic_lib');
+
+
+
 ?>
